85d729c97342b21b0b6dc1fa9a6f5a3b8221ed3e
shapovalov-k <shapovalovkv@gmail.com>
2018-10-19 17:35:56 +0200

    task 28-1

--- main.rb
+++ main.rb
@@ -16,7 +16,15 @@ require_relative 'lib/product'
 require_relative 'lib/film'
 require_relative 'lib/book'
 
+products = []
 
-film = Film.new(price: 290, amount: 4)
+products << Film.new(title: "Leon", year: 1994, director: "Люк Бессон", price: 990, amount: 5)
+products << Film.new(title: "Дурак", year: 2014, director: "Юрий Быков", price: 390, amount: 1)
 
-puts "фильм Леон стоит #{film.price} рублей,"
+# название, жанр и автора
+products << Book.new(title: "Идиот", genre: "роман", author: "Федор Достоевский", price: 1500, amount: 10)
+
+# Выводим все продукты в консоль просто передавая их методу puts
+puts 'Вот какие товары у нас есть:'
+puts
+products.each { |product| puts product }
